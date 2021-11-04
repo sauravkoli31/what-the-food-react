06bfc8df52c6a3ccfec3f537ad40b18d84b9db7b
sauravkoli31 <nasaspace91@gmail.com>
2021-11-04 04:29:18 +0400

    Added more cards into view to show scrapped data
    To show the scrapped data for the selected restaurant. This includes the following new components added :
    
    average delivery time
    minimum order
    rating
    status
    delivery time
    delivery fee
    most selling items prices

--- src/components/RestaurantView.js
+++ src/components/RestaurantView.js
@@ -9,14 +9,48 @@ import {
 import Divider from "@mui/material/Divider";
 import { useSelector, useDispatch } from "react-redux";
 import placeholderHeroBanner from "../static/images/placeholder_image1-900x568.jpg";
-import { setSelectedRestaurantLink } from "../redux/userData";
+import {
+  setSelectedRestaurantLink,
+  setSelectedRestaurantData,
+} from "../redux/userData";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+function MiniInfoCards(props) {
+  return (
+    <Card
+      variant="outlined"
+      sx={{
+        padding: "0.5rem",
+        marginX: "0.3rem",
+        width: "fit-content",
+        backgroundColor: "#8d8d8d",
+      }}
+    >
+      <Typography
+        // variant="subtitle2"
+        sx={{
+          // fontWeight: 800,
+          color: "white",
+          fontSize: "0.675em",
+          letterSpacing: "0.2em",
+        }}
+      >
+        {props?.title.toUpperCase()} : {props?.value}
+      </Typography>
+    </Card>
+  );
+}
 
 function RestaurantView() {
-    const userData = useSelector((state) => state.userData);
-    let topSellingItemPresent =
-      userData?.selectedRestaurant?.mostSellingItems?.length > 0;
+  const userData = useSelector((state) => state.userData);
+  const myRef = useRef(null);
+  const dispatch = useDispatch();
+  const executeScroll = () => myRef.current.scrollIntoView();
 
-      const dispatch = useDispatch();
+  let topSellingItemPresent =
+    userData?.selectedRestaurant?.mostSellingItems?.length > 0;
+
   useEffect(() => {
     let urlencoded = new URLSearchParams();
     urlencoded.append("latitude", userData?.location?.lat);
@@ -35,9 +69,26 @@ function RestaurantView() {
       .catch((error) => console.error(error));
   }, [userData?.selectedRestaurant]);
 
-  const myRef = useRef(null)
-
-  const executeScroll = () => myRef.current.scrollIntoView()    
+  useEffect(() => {
+    dispatch(setSelectedRestaurantData(null));
+    if (userData?.selectedRestaurantLink) {
+      let mostSellingItems = userData?.selectedRestaurant?.mostSellingItems.map(item => item.id)
+      let urlencoded = new URLSearchParams();
+      urlencoded.append("url", userData?.selectedRestaurantLink);
+      urlencoded.append("mSI", mostSellingItems);
+      let options = {
+        method: "POST",
+        body: urlencoded,
+      };
+      let url = `${process.env.REACT_APP_API_SERVER}/getRestaurantData`;
+      fetch(url, options)
+        .then((data) => data.json())
+        .then((data) => {
+          dispatch(setSelectedRestaurantData(data));
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [userData?.selectedRestaurantLink]);
 
   return (
     <div
@@ -51,31 +102,108 @@ function RestaurantView() {
         className="curved-edges glassmorph"
         sx={{ margin: "20px", height: "100%" }}
       >
-        <div className="foodImages " style={{ position: "relative" }}>
+        <div style={{ position: "relative" }}>
           <CardActionArea
             onClick={() =>
               userData?.selectedRestaurantLink &&
               window.open(userData?.selectedRestaurantLink, "_blank")
             }
           >
-            <CardMedia
-              component="img"
-              image={userData?.selectedRestaurant?.heroImage}
-              onError={(e) => {
-                e.target.onerror = null;
-                e.target.src = placeholderHeroBanner;
-              }}
-              onLoad={executeScroll}
-              sx={{
-                maxHeight: "320px",
-              }}
-            ></CardMedia>
+            <div className="foodImages">
+              <CardMedia
+                component="img"
+                image={userData?.selectedRestaurant?.heroImage}
+                onError={(e) => {
+                  e.target.onerror = null;
+                  e.target.src = placeholderHeroBanner;
+                }}
+                onLoad={executeScroll}
+                sx={{
+                  maxHeight: "320px",
+                }}
+              ></CardMedia>
+            </div>
+            {userData?.selectedRestaurantData?.status && (
+              <div
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  right: 0,
+                  margin: "1rem",
+                  display: "flex"
+                }}
+                >
+                <Card
+                  sx={{
+                    padding: "0.5rem",
+                    display: "inline-flex",
+                    alignItems: "center",
+                    marginLeft: "0.5rem",
+                  }}
+                  className={`defaultBg ${userData?.selectedRestaurantData?.status?.toLowerCase()}`}
+                  >
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      fontWeight: 800,
+                      color: "white",
+                      fontSize: "1em",
+                    }}
+                    >
+                    {userData?.selectedRestaurantData?.status.toUpperCase()}
+                  </Typography>
+                </Card>
+                <Card
+                  sx={{
+                    marginLeft: "0.5rem",
+                    padding: "0.5rem",
+                    display: "inline-flex",
+                    alignItems: "center",
+                    backgroundColor: "orange",
+                  }}
+                >
+                  <StarIcon sx={{ fontSize: "1em", color: "white" }} />
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      fontWeight: 800,
+                      color: "white",
+                      fontSize: "1em",
+                    }}
+                  >
+                    {userData?.selectedRestaurantData?.rate > 0 ? userData?.selectedRestaurantData?.rate : "NA" }
+                  </Typography>
+                </Card>
+                <Card
+                  sx={{
+                    marginLeft: "0.5rem",
+                    padding: "0.5rem",
+                    display: "inline-flex",
+                    alignItems: "center",
+                    backgroundColor: "coral",
+                  }}
+                >
+                  <AccessTimeIcon sx={{ color: "white" }} />
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      fontWeight: 800,
+                      color: "white",
+                      fontSize: "1em",
+                    }}
+                  >
+                    {userData?.selectedRestaurantData?.deliverySchedule}
+                  </Typography>
+                </Card>
+              </div>
+            )}
           </CardActionArea>
         </div>
         <div
           style={{
             background: "rgb(255,255,255)",
-            background: "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
+            background:
+              "linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
           }}
         >
           <CardContent
@@ -109,13 +237,47 @@ function RestaurantView() {
                 {userData?.selectedRestaurant?.name?.toUpperCase()}
               </Typography>
             </div>
-            <Typography component="div" variant="body1" align="left">
+          </CardContent>
+          <div
+            style={{
+              flex: "1 1 auto",
+              textAlign: "-webkit-left",
+              display: "flex",
+              flexWrap: "wrap",
+              paddingLeft: "16px",
+              paddingRight: "16px",
+            }}
+          >
+            {userData?.selectedRestaurantData && (
+              <>
+                <MiniInfoCards
+                  title="Min Order"
+                  value={`AED ${userData?.selectedRestaurantData?.minimumOrderAmount}`}
+                />
+                <MiniInfoCards
+                  title="Delivery Fee"
+                  value={`AED ${userData?.selectedRestaurantData?.deliveryFee}`}
+                />
+                <MiniInfoCards
+                  title="Avg Delivery Time"
+                  value={userData?.selectedRestaurantData?.avgDeliveryTime}
+                />
+              </>
+            )}
+            <Typography
+              component="div"
+              variant="body1"
+              align="left"
+              sx={{
+                marginTop: "16px",
+              }}
+            >
               {userData?.selectedRestaurant?.description?.replace(
                 /<br \/>/g,
                 ""
               )}
             </Typography>
-          </CardContent>
+          </div>
         </div>
         {/* 
         Top Selling items.
@@ -142,6 +304,13 @@ function RestaurantView() {
         <div className={"mainContent foodContent"} style={{ margin: "4px" }}>
           {userData?.selectedRestaurant?.mostSellingItems?.map(
             (data, dataKey) => {
+              let mSIPrice = 0
+              let showPrice = false
+              if (userData?.selectedRestaurantData?.mostSellingItemsPrices.length > 1){
+                let mostSellingItem = userData?.selectedRestaurantData?.mostSellingItemsPrices?.filter(food => food?.id === data?.id)
+                mSIPrice = mostSellingItem[0]?.price || "NA"
+                showPrice = true
+              }
               return (
                 <Card
                   className="curved-edges foodImages foodImages_inner"
@@ -175,6 +344,31 @@ function RestaurantView() {
                     >
                       {data?.name}
                     </Typography>
+                  </CardContent>
+                  <CardContent
+                    className="mainContent"
+                    sx={{
+                      position: "absolute",
+                      top: 0,
+                      right:0 ,
+                      color: "white",
+                    }}
+                  >
+                    <Card>
+                    <Typography
+                      component="div"
+                      variant="button"
+                      sx={{
+                        lineHeight: 1.2,
+                        padding: 0.7
+                      }}
+                    >
+                      {showPrice && `AED ${mSIPrice}`}
+
+
+
+                    </Typography>
+                    </Card>
                   </CardContent>
                 </Card>
               );
